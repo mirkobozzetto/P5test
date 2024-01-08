c83ef2e9e304f40e5aa44ae528864f4c83b46dbf
Mirko Bozzetto <mirkouch@gmail.com>
2024-01-08 23:52:57 +0100

    Refactor grid setup and add drawing functionality

--- sketch.js
+++ sketch.js
@@ -7,10 +7,15 @@ function make2DArray(cols, rows) {
 }
 
 let grid;
-let cols = 10;
-let rows = 10;
+let cols;
+let rows;
+let resolution = 20;
 
 function setup() {
+  createCanvas(200, 200);
+  cols = width / resolution;
+  rows = height / resolution;
+
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
@@ -19,4 +24,16 @@ function setup() {
   }
 }
 
-console.table(grid);
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+      }
+      rect(x, y, resolution, resolution);
+    }
+  }
+}
